a338a736270dd184e482ef7ce94ca3a8e9718b1a
JrValerio <amarovsjr81@gmail.com>
2024-11-14 00:50:44 -0300

    feat(ui): add Header component to main layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<h1>Welcome to EcoVoz Frontend</h1>}/>
-      </Routes>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<h1>Welcome to EcoVoz Frontend</h1>} />
+        </Routes>
+      </div>
     </Router>
   )
 }
