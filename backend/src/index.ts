83516e0cf968a67118b8fbb6e79bd575972bca10
JrValerio <amarovsjr81@gmail.com>
2024-11-15 01:49:41 -0300

    feat: configure MongoDB connection and add user authentication setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,10 +2,13 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import statusRoutes from './routes/status';
+import connectDB from './config/database';
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 app.use(cors());
 app.use(express.json());
