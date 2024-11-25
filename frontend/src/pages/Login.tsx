813505683747ca62c8f2ea94b5e6a3f9bafeb78a
JrValerio <amarovsjr81@gmail.com>
2024-11-25 10:13:35 -0300

    chore(project): update package.json files and dependencies

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 import { GoogleLogin, CredentialResponse } from '@react-oauth/google';
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { toast } from 'react-toastify';
 import { useTranslation } from 'react-i18next';
 import Alert from '../components/Alert';
 import GoogleAuthProvider from '../providers/GoogleAuthProvider';
