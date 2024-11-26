663f2ca372e264ede2119da5f42d64e176aa39de
JrValerio <amarovsjr81@gmail.com>
2024-11-26 05:31:29 -0300

    fix: resolve Vite config issue with require.resolve

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,22 +12,7 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
       '@shared': path.resolve(__dirname, '../shared'),
-      'i18next-browser-languagedetector': require.resolve('i18next-browser-languagedetector'),
-      'i18next-http-backend': require.resolve('i18next-http-backend'),
-      'i18next': require.resolve('i18next'),
-      'react-i18next': require.resolve('react-i18next'),
-      'react-redux': require.resolve('react-redux'),
-      'react-toastify': require.resolve('react-toastify'),
-      'redux': require.resolve('redux'),
-      'redux-thunk': require.resolve('redux-thunk'),
-      'react-router-dom': require.resolve('react-router-dom'),
-      'react-router-dom/server': require.resolve('react-router-dom/server'),
-      'react-router-dom/client': require.resolve('react-router-dom/client'),
-      'react-router-dom/server/matchRoutes': require.resolve('react-router-dom/server/matchRoutes'),
-      'react-router-dom/server/getRouteParams': require.resolve('react-router-dom/server/getRouteParams'),
-      'react-router-dom/server/renderRoutes': require.resolve('react-router-dom/server/renderRoutes'),
-      'react-router-dom/server/renderMatches': require.resolve('react-router-dom/server/renderMatches'),
-      
+      'i18next-browser-languagedetector': 'i18next-browser-languagedetector',      
     },
   },
   base: './', // Define o diretório raiz do projeto
